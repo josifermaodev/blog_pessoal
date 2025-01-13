9ba5504bec9c7eade3878d145ea6740381151c5d
josifermaodev <josianefermao@gmail.com>
2025-01-13 12:15:42 -0300

    Blog Passoal Nest

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Habilitando globalmente a validação de dados
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Habilitando CORS na aplicação
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
+
+
