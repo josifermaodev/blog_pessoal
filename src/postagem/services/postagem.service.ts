aa1af6d42b4907e13d66dd8ef1d45b2a1af422c3
josifermaodev <josianefermao@gmail.com>
2025-01-14 15:35:48 -0300

    Adição dos metodos GET, Post, Put e Delete

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class PostagemService{
@@ -14,5 +14,45 @@ export class PostagemService{
     async findAll(): Promise<Postagem[]> {
         return await this.postagemRepository.find(); // SELECT * FROM tb_postagem;
     }
+
+    // SELECT * FROM tb_postagens WHERE id = ?;
+    async findById(id: number): Promise<Postagem> {
+       let postagem = await this.postagemRepository.findOne({
+        where: {
+            id
+        }
+       });
+
+       if (!postagem)
+        throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+       return postagem;
+    }
+
+    async findByTitulo(titulo: string): Promise<Postagem[]> {
+        return await this.postagemRepository.find({
+            where:{
+                titulo: ILike(`%${titulo}%`) // ILike - insensitivo, vai ignorar o banco de dados
+            }
+        })
+    }
+
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem); // INSERT INTO tb_postagem (titulo, texto, data) VALUES (?,?)
+    }
+
+    async update(postagem: Postagem): Promise<Postagem> {
+        await this.findById(postagem.id)
+
+        //UPDATE tb_postagens SET titulo = ?, texto = ? WHERE id = postagem.id
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+
+        return await this.postagemRepository.delete(id);
+    }
 }
+
 
