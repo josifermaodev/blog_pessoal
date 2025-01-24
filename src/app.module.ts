5de5a5ad2bceb49862fc267ae3dda372646f7fa6
alanbrunoscience <alanbrunomath@gmail.com>
2025-01-24 12:18:24 -0300

    :refactor: Fix relatives' paths on files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
 import { Postagem } from './postagem/entities/postagem.entity';
-import { TemaModule } from './temas/tema.module';
-import { Temas } from './temas/entities/tema.entity';
+import { TemaModule } from './tema/tema.module';
+import { Temas } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { Usuario } from './usuario/entities/usuario.entity';
@@ -26,7 +24,7 @@ import { Usuario } from './usuario/entities/usuario.entity';
     PostagemModule,
     TemaModule,
     AuthModule,
-    UsuarioModule
+    UsuarioModule,
   ],
   controllers: [],
   providers: [],
