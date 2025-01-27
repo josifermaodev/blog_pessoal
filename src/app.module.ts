f00968490794eb39d23687f7f6e00b25a7370e1f
josifermaodev <josianefermao@gmail.com>
2025-01-27 11:39:20 -0300

    correção de dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
